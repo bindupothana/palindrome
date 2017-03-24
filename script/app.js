850265c0e022ec6f0ea0ec602d6400f757b96006
bindupothana <himabindu.pothana@navaratan.com>
2017-03-24 19:32:00 +0530

    palindrome

--- script/app.js
+++ script/app.js
@@ -1,19 +1,21 @@
 var app=angular.module('palindromeApp',[]);
 app.controller('palindromeCtrl',function($scope){
-	$scope.palindrome=function(num){
-		var temp =0;
-		var i;
-		while( i!=0 )
-    {
-        $scope.remainder = num%10;
-        // console.log("num",num)
-       $scope.reverseInteger =$scope.reversedInteger*10 + $scope.remainder;
-               console.log("num", $scope.reverseInteger)
-
-        num / 10;
-    }
-
-	}
-
+	$scope.palindrome=function(string){
+    var n,r,sum=0,temp;    
+         
+         
+          temp=n;      
+          while(n>0)      
+          {      
+           r=n%10;      
+           sum=(sum*10)+r;      
+           n=n/10;      
+          }      
+          if(temp==sum)      
+           alert("Number is Palindrome.");      
+          else      
+           alert("Number is not Palindrome");     
+    }  
+  
 
 });
